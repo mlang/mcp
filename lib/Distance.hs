791722f5dca0569deab87528d272c13f834ded3e
Mario Lang <mlang@blind.guru>
2019-03-13 00:35:33 +0100

    ExternalIP

--- lib/Distance.hs
+++ lib/Distance.hs
@@ -14,8 +14,13 @@ instance Distance GPS.Point GPS.Distance where
 instance Distance ICAO.ICAOCode GPS.Distance where
   distance p q = GPS.distance (ICAO.point p) (ICAO.point q)
 
-newtype IP = IP String
+data IP = IP String | ExternalIP
 
 instance Distance IP (IO GPS.Distance) where
   distance (IP p) (IP q) = GPS.distance <$> (GeoIP.point <$> (GeoIP.whereIs p))
                                         <*> (GeoIP.point <$> (GeoIP.whereIs q))
+  distance ExternalIP (IP q) = GPS.distance <$> (GeoIP.point <$> GeoIP.whereAmI)
+                                            <*> (GeoIP.point <$> (GeoIP.whereIs q))
+  distance (IP p) ExternalIP = GPS.distance <$> (GeoIP.point <$> (GeoIP.whereIs p))
+                                            <*> (GeoIP.point <$> GeoIP.whereAmI)
+  distance ExternalIP ExternalIP = pure 0
