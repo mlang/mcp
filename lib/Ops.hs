62cc0b0e923a8709e94bce4e8fc4653c53022f68
Mario Lang <mlang@blind.guru>
2019-03-16 18:24:58 +0100

    betris

--- lib/Ops.hs
+++ lib/Ops.hs
@@ -1,46 +1,14 @@
 {-# LANGUAGE DefaultSignatures, GADTs #-}
-module Ops (cd, glob, draw, printBanner) where
+module Ops (glob, draw, printBanner, tetris) where
 
+import Command.Betris (betris)
+import qualified Command.Betris as Betris (Options(..))
 import Data.String
+import Data.Time.Units (fromMicroseconds)
 import Diagrams
 import Diagrams.Backend.Braille
 import qualified System.FilePath.Glob as Glob
 import System.Directory
-
-class CDArg a where
-  asArg :: a -> [String]
-  default asArg :: Show a => a -> [String]
-  asArg a = [show a]
-  asArgFromList :: [a] -> [String]
-  default asArgFromList :: Show a => [a] -> [String]
-  asArgFromList = concatMap asArg
-
-instance CDArg Bool
-instance CDArg Int
-instance CDArg Integer
-instance CDArg Word
-
-instance CDArg Char where
-  asArg s = [[s]]
-  asArgFromList s = [s]
-
-instance CDArg a => CDArg [a] where
-  asArg = asArgFromList
-  asArgFromList = concatMap asArg
-
-class CDResult r where
-  changeDirectory :: [String] -> r
-
-instance (a ~ ()) => CDResult (IO a) where
-  changeDirectory [] = setCurrentDirectory =<< getHomeDirectory
-  changeDirectory [dir] = setCurrentDirectory dir
-  changeDirectory _ = putStrLn "Too many arguments"
-
-instance (CDArg a, CDResult r) => CDResult (a -> r) where
-  changeDirectory args dir = changeDirectory $ args <> asArg dir
-
-cd :: CDResult r => r
-cd = changeDirectory mempty
 
 draw :: Diagram Braille -> IO ()
 draw = putStr . renderDia Braille (BrailleOptions (mkWidth 80))
@@ -56,3 +24,6 @@ printBanner = draw $
  <> translateY (-0.7)  (translateX (-0.25) (baselineText "Program"))
 
 glob pattern = traverse makeRelativeToCurrentDirectory =<< Glob.glob pattern
+
+tetris :: IO ()
+tetris = betris (Betris.Options (fromMicroseconds 1000000))
